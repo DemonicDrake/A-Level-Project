38a41725754771f3211afb7188f4858d17d19816
DemonicDrake <61542853+DemonicDrake@users.noreply.github.com>
2022-10-19 09:46:21 +0100

    fixed fullscreen toggle and added finished movement

--- main.py
+++ main.py
@@ -38,65 +38,56 @@ if __name__ == "__main__":
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
-    square_speed = 160
-    # Setup for the interval example
-
-    tick_interval = 1.000
-    tick_time = 0
-
-    # Change this to see how different framerates result in consistent
-    # motion. Try values like 50 or 200
-
-    delay_duration = 5
-
     running = True
     while running:
-        # Interval example
-
-        tick_time += timer.get_frame_duration()
-        if tick_time > tick_interval:
-            tick_time = 0
-            print
-            "Tick"
-
-        # Motion example
-        # display.blit(black_square, (square_x, 0))
-        # square_x += (square_speed * timer.get_frame_duration())
-        # if square_x > 320:
-        #     square_x = 0
-        # display.blit(red_square, (square_x, 0))
-
-        # Insert artificial delay
-
-        pygame.time.delay(delay_duration)
-
-        # Must call this every frame
-
         timer.tick()
 
+        screen.fill((0, 0, 0))
+
         for event in pygame.event.get():
+
+            # x button
             if event.type == pygame.QUIT:
                 running = False
 
-            keys = pygame.key.get_pressed()
+            # full-screen toggle
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:
+                    if screen.get_flags() & FULLSCREEN:
+                        pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+                    else:
+                        pygame.display.set_mode((get_monitors()[0].width, get_monitors()[0].height), FULLSCREEN)
 
-            # full-screen toggle (also dont work)
-            if keys[pygame.K_F11]:
-                if screen.get_flags() & pygame.FULLSCREEN:
-                    pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-                else:
-                    pygame.display.set_mode((get_monitors()[0].width, get_monitors()[0].height), pygame.FULLSCREEN)
+        # movement check
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            constants.x -= constants.VELOCITY * timer.get_frame_duration()
+        if keys[pygame.K_RIGHT]:
+            constants.x += constants.VELOCITY * timer.get_frame_duration()
+        if keys[pygame.K_UP]:
+            constants.y -= constants.VELOCITY * timer.get_frame_duration()
+        if keys[pygame.K_DOWN]:
+            constants.y += constants.VELOCITY * timer.get_frame_duration()
 
-            # dont work
-            if keys[pygame.K_LEFT]:
-                constants.VELOCITY -= square_speed * timer.get_frame_duration()
-            if keys[pygame.K_RIGHT]:
-                constants.VELOCITY += square_speed * timer.get_frame_duration()
-            if keys[pygame.K_UP]:
-                constants.VELOCITY -= square_speed * timer.get_frame_duration()
-            if keys[pygame.K_DOWN]:
-                constants.VELOCITY += square_speed * timer.get_frame_duration()
+        # screen edge collision check
+        if screen.get_flags() & FULLSCREEN:
+            if constants.x < 0:
+                constants.x = 0
+            if constants.x + constants.WIDTH > get_monitors()[0].width:
+                constants.x = get_monitors()[0].width - constants.WIDTH
+            if constants.y < 0:
+                constants.y = 0
+            if constants.y + constants.HEIGHT > get_monitors()[0].height:
+                constants.y = get_monitors()[0].height - constants.HEIGHT
+        else:
+            if constants.x < 0:
+                constants.x = 0
+            if constants.x + constants.WIDTH > constants.SCREEN_WIDTH:
+                constants.x = constants.SCREEN_WIDTH - constants.WIDTH
+            if constants.y < 0:
+                constants.y = 0
+            if constants.y + constants.HEIGHT > constants.SCREEN_HEIGHT:
+                constants.y = constants.SCREEN_HEIGHT - constants.HEIGHT
 
-        pygame.draw.rect(screen, (255, 0, 0), (constants.x, constants.y, constants.WIDTH, constants.HEIGHT))
-
+        pygame.draw.rect(screen, (255, 255, 255), (constants.x, constants.y, constants.WIDTH, constants.HEIGHT))
         pygame.display.update()
