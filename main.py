74996b44125810c2e1ea544e9059e20ca8f76761
DemonicDrake <61542853+DemonicDrake@users.noreply.github.com>
2022-11-01 19:11:55 +0000

    e

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from screeninfo import get_monitors
 import constants
 
 bg = pygame.image.load('assets/City Background/Sky.png')
+bg = pygame.transform.scale(bg, (get_monitors()[0].width * 1.5, get_monitors()[0].height * 1.5))
+bg1 = pygame.image.load('assets/City Background/City Background.png')
+bg1 = pygame.transform.scale(bg1, (get_monitors()[0].width * 1.5, get_monitors()[0].height * 1.5))
+bg2 = pygame.image.load('assets/City Background/City Foreground.png')
+bg2 = pygame.transform.scale(bg2, (get_monitors()[0].width * 1.5, get_monitors()[0].height * 1.5))
+
 
 class FpsClock:
     def __init__(self):
@@ -53,12 +59,12 @@ if __name__ == "__main__":
                 running = False
 
             # full-screen toggle
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_F11:
-                    if screen.get_flags() & FULLSCREEN:
-                        pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-                    else:
-                        pygame.display.set_mode((get_monitors()[0].width, get_monitors()[0].height), FULLSCREEN)
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_F11:
+            #         if screen.get_flags() & FULLSCREEN:
+            #             pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+            #         else:
+            #             pygame.display.set_mode((get_monitors()[0].width, get_monitors()[0].height), FULLSCREEN)
 
         # movement check
         keys = pygame.key.get_pressed()
@@ -95,6 +101,8 @@ if __name__ == "__main__":
             if constants.y + constants.HEIGHT > constants.SCREEN_HEIGHT:
                 constants.y = constants.SCREEN_HEIGHT - constants.HEIGHT
 
-        screen.blit(bg, (0, 0))
+        #screen.blit(bg, (0, 0))
+        screen.blit(bg1, (0, 0))
+        screen.blit(bg2, (0, 0))
         pygame.draw.rect(screen, (255, 255, 255), (constants.x, constants.y, constants.WIDTH, constants.HEIGHT))
         pygame.display.update()
